6b16ed79c92da7861a820f2a1eebd6d3f90315d5
Hendrik Schreiber <hs@tagtraum.com>
2021-09-06 17:15:47 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+[![LGPL 2.1](https://img.shields.io/badge/License-LGPL_2.1-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.tagtraum/obstunes/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.tagtraum/obstunes)
+[![Build and Test](https://github.com/hendriks73/obstunes/workflows/Build%20and%20Test/badge.svg)](https://github.com/hendriks73/obstunes/actions)
+
+
+# Obstunes
+
+*Obstunes* is a Java API for iTunes (macOS) based on
+[JaplScript](https://github.com/hendriks73/japlscript).
+
+
+## Installation
+
+Obstunes is released via [Maven](https://maven.apache.org).
+You can install it via the following dependency:
+
+```xml
+<dependency>
+    <groupId>com.tagtraum</groupId>
+    <artifactId>obstunes</artifactId>
+</dependency>
+```
+
+The Maven artifacts also contain sources and javadocs. 
+
+If you are using [modules](https://en.wikipedia.org/wiki/Java_Platform_Module_System),
+its name is `tagtraum.obstunes`.
+
+
+## Usage
+                           
+To use the generated code, do something like this:
+
+```java
+import com.tagtraum.macos.itunes.Application;
+
+public class PlayPause {
+
+    public static void main(final String[] args) {
+        Application app = Application.getInstance();
+        // then use app, for example, to toggle playback (if a track is in the player)
+        app.playpause();
+    }
+}
+```
---
+++ src/test/java/com/tagtraum/macos/itunes/TestApplication.java
@@ -0,0 +1,53 @@
+/*
+ * =================================================
+ * Copyright 2021 tagtraum industries incorporated
+ * All rights reserved.
+ * =================================================
+ */
+package com.tagtraum.macos.itunes;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * TestApplication.
+ *
+ * @author <a href="mailto:hs@tagtraum.com">Hendrik Schreiber</a>
+ */
+public class TestApplication {
+
+    @Test
+    public void testVersion() {
+        final Application application = Application.getInstance();
+        final String version = application.getVersion();
+        assertNotNull(version);
+        assertFalse(version.trim().isEmpty());
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "os.version",
+        matches = "10\\.14\\..*",
+        disabledReason = "Test only enabled on macOS 10.14")
+    public void testLibraryPlaylistCount() {
+        final Application application = Application.getInstance();
+        final LibraryPlaylist libraryPlaylist = application.getSources()[0].getLibraryPlaylists()[0];
+        System.out.println("Tracks: " + libraryPlaylist.countTracks());
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "os.version",
+        matches = "10\\.14\\..*",
+        disabledReason = "Test only enabled on macOS 10.14")
+    public void testCurrentlyPLaying() {
+        final Application application = Application.getInstance();
+        final Track currentTrack = application.getCurrentTrack();
+        if (currentTrack == null) {
+            System.out.println("No track loaded.");
+        } else {
+            System.out.println("Current track: " + currentTrack.getName() + " by " + currentTrack.getArtist());
+        }
+    }
+}
